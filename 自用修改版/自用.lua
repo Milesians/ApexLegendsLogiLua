7d88ff734e80f576d5807a2170fc855d3ebdaaad
milesians <huangtao3001@gmail.com>
2023-02-02 21:45:47 +0800

    feat:add myself

---
+++ 自用修改版/自用.lua
@@ -0,0 +1,92 @@
+---【长按】----------------------------------------------------------------------------------------------
+LMD = 1.9
+--输入游戏内鼠标灵敏度（设置你游戏内的灵敏度如：3.5就改3.5
+ADS = 1.0
+--ADS输入游戏内ADS鼠标灵敏度,也叫开镜灵敏度，默认1.0（游戏内ads改1.3效果更好）
+---------------------------------------------------------------------------------------------------------
+ChangeArmorButton= 5
+--设置一键换甲鼠标按键
+UseKey = "e"
+--设置使用按键
+---------------------------------------------------------------------------------------------------------
+PressGun = false
+--设置是否需要压枪
+Y = {53933,47409,40700,25003,4411}
+--换甲坐标--按中键获取
+---------------------------------
+Frequency = 6
+Adjustment = 0
+--------------------------------------
+
+--LeftRightRange = (6 // (LMD * ADS)) + 3 + Adjustment
+LeftRightRange = 3
+Decline_range = 8 * LMD
+
+-- utils start
+function BetterSleep(t)
+    local sleep_start_time = GetRunningTime()
+    repeat
+        local sleep_end_time = GetRunningTime()
+        local sleep_elapsed = sleep_end_time - sleep_start_time
+    until (sleep_elapsed > t)
+end
+-- utils end
+-- 换甲 
+
+
+function ChangeArmor()
+    for key, value in ipairs(Y) do
+        MoveMouseTo(12909, value )
+        Sleep(13)
+        PressAndReleaseMouseButton(1)
+        Sleep(13)
+    end
+    PressAndReleaseKey("escape")
+end
+
+
+EnablePrimaryMouseButtonEvents(true)
+
+function OnEvent(event, arg)
+    if (event == "MOUSE_BUTTON_PRESSED" and arg == 3) then
+        -- 初始化：获取坐标
+        x,y = GetMousePosition()
+        OutputLogMessage("X: %d || Y: %d\n",x,y)
+    end
+
+
+    if (event == "MOUSE_BUTTON_PRESSED" and arg == ChangeArmorButton) then
+        PressKey(UseKey)
+        Sleep(500)
+        ReleaseKey(UseKey)
+        Sleep(50)
+        ChangeArmor()
+    end
+    -- 换甲 end
+
+    local time = 0
+    local fast_decline = LeftRightRange * 12
+
+    if (IsKeyLockOn("capslock")) then
+        while (IsMouseButtonPressed(3)) do
+            if (IsMouseButtonPressed(1)) then
+                MoveMouseRelative(LeftRightRange, -LeftRightRange)
+                BetterSleep(Frequency)
+                time = time + Frequency
+                MoveMouseRelative(-LeftRightRange, LeftRightRange)
+                BetterSleep(Frequency)
+
+                time = time + Frequency
+                if (time >= Decline_range and PressGun) then
+                    MoveMouseRelative(0, ((fast_decline // 12) + 1))
+                    time = 0
+                    if (fast_decline > 0) then
+                        fast_decline = fast_decline - 2
+                    else
+                        fast_decline = 0
+                    end
+                end
+            end
+        end
+    end
+end
