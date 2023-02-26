ee2cd551db33da8d0935f4dbaffe36c5aa0d1265
milesians <37968554+Milesians@users.noreply.github.com>
2023-02-26 12:34:06 +0800

    update:自用

--- 自用修改版/自用.lua
+++ 自用修改版/自用.lua
@@ -1,16 +1,9 @@
----【长按】----------------------------------------------------------------------------------------------
-LMD = 1.9
---输入游戏内鼠标灵敏度（设置你游戏内的灵敏度如：3.5就改3.5
-ADS = 1.0
---ADS输入游戏内ADS鼠标灵敏度,也叫开镜灵敏度，默认1.0（游戏内ads改1.3效果更好）
 ---------------------------------------------------------------------------------------------------------
 ChangeArmorButton= 5
 --设置一键换甲鼠标按键
 UseKey = "e"
 --设置使用按键
 ---------------------------------------------------------------------------------------------------------
-EnableShake = true
---设置是否需要抖动
 PressGun = false
 --设置是否需要压枪
 DisableCapsLock= true
@@ -18,14 +11,12 @@ DisableCapsLock= true
 Y = {53933,46271,47409,44085,42400,40700,25003,4411}
 --换甲坐标--按中键获取
 ---------------------------------
-Frequency = 20
-Adjustment = 0
+Frequency = 5
 --------------------------------------
 
 --LeftRightRange = (6 // (LMD * ADS)) + 3 + Adjustment
-Range = 1
+Range = 2
 Decline = 2
-Decline_range = 8 * LMD
 
 -- utils start
 function BetterSleep(t)
@@ -42,11 +33,11 @@ end
 function ChangeArmor()
     for key, value in ipairs(Y) do
         MoveMouseTo(12909, value )
-        Sleep(13)
+        BetterSleep(13)
         PressAndReleaseMouseButton(1)
-        Sleep(13)
+        BetterSleep(13)
     end
-    PressAndReleaseKey("escape")
+    PressAndReleaseKey("TAB")
 end
 
 
@@ -62,9 +53,9 @@ function OnEvent(event, arg)
 
     if (event == "MOUSE_BUTTON_PRESSED" and arg == ChangeArmorButton) then
         PressKey(UseKey)
-        Sleep(500)
+        BetterSleep(500)
         ReleaseKey(UseKey)
-        Sleep(50)
+        BetterSleep(50)
         ChangeArmor()
     end
     -- 换甲 end
@@ -72,19 +63,19 @@ function OnEvent(event, arg)
 
     if (IsKeyLockOn("capslock") or DisableCapsLock) then
         while (IsMouseButtonPressed(3)) do
-            while (IsMouseButtonPressed(1)) do
+            if(IsMouseButtonPressed(1)) then
 
-                MoveMouseRelative(-Range, Range * 2)
+                MoveMouseRelative(-Range, Range )
                 BetterSleep(Frequency)
-                MoveMouseRelative(Range, -Range * 2)
+                MoveMouseRelative(Range, -Range )
                 BetterSleep(Frequency)
-                MoveMouseRelative(-Range, Range * 2)
+                MoveMouseRelative(-Range, Range )
                 BetterSleep(Frequency)
-                MoveMouseRelative(Range, -Range * 2)
+                MoveMouseRelative(Range, -Range )
                 BetterSleep(Frequency)
-                MoveMouseRelative(-Range, Range * 2)
+                MoveMouseRelative(-Range, Range )
                 BetterSleep(Frequency)
-                MoveMouseRelative(Range, -Range * 2)
+                MoveMouseRelative(Range, -Range )
                 BetterSleep(Frequency)
                 if (PressGun) then
                     MoveMouseRelative(0, Decline)
