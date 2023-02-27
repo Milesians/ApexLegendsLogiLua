bed5f6a9430c656a9abc82819ee39e07b1cdcd99
milesians <37968554+Milesians@users.noreply.github.com>
2023-02-27 09:51:19 +0800

    feat:开关镜检测 close #2

--- 自用修改版/自用.lua
+++ 自用修改版/自用.lua
@@ -1,22 +1,22 @@
 ---------------------------------------------------------------------------------------------------------
-ChangeArmorButton= 5
---设置一键换甲鼠标按键
+ChangeArmorButton = 5
+--设置一键换甲鼠标按键 5是上侧键 4是下侧键
 UseKey = "e"
 --设置使用按键
 ---------------------------------------------------------------------------------------------------------
 PressGun = false
 --设置是否需要压枪
-DisableCapsLock= true
+DisableCapsLock = true
 --是否始终开启抖枪
-Y = {53933,46271,47409,44085,42400,40700,25003,4411}
+Y = {53933, 46271, 47409, 44085, 42400, 40700, 25003, 4411}
 --换甲坐标--按中键获取
 ---------------------------------
 Frequency = 5
 --------------------------------------
-
---LeftRightRange = (6 // (LMD * ADS)) + 3 + Adjustment
 Range = 2
+--控制上下左右的幅度
 Decline = 2
+--控制压枪的幅度
 
 -- utils start
 function BetterSleep(t)
@@ -27,12 +27,11 @@ function BetterSleep(t)
     until (sleep_elapsed > t)
 end
 -- utils end
--- 换甲 
-
+-- 换甲
 
 function ChangeArmor()
     for key, value in ipairs(Y) do
-        MoveMouseTo(12909, value )
+        MoveMouseTo(12909, value)
         BetterSleep(13)
         PressAndReleaseMouseButton(1)
         BetterSleep(13)
@@ -40,16 +39,16 @@ function ChangeArmor()
     PressAndReleaseKey("TAB")
 end
 
-
-EnablePrimaryMouseButtonEvents(true)
-
 function OnEvent(event, arg)
-    if (event == "MOUSE_BUTTON_PRESSED" and arg == 3) then
-        -- 初始化：获取坐标
-        x,y = GetMousePosition()
-        OutputLogMessage("X: %d || Y: %d\n",x,y)
+    if (event == "PROFILE_ACTIVATED") then
+        EnablePrimaryMouseButtonEvents(true)
     end
 
+    if (event == "MOUSE_BUTTON_PRESSED" and arg == 3) then
+        -- 初始化：获取坐标
+        x, y = GetMousePosition()
+        OutputLogMessage("X: %d || Y: %d\n", x, y)
+    end
 
     if (event == "MOUSE_BUTTON_PRESSED" and arg == ChangeArmorButton) then
         PressKey(UseKey)
@@ -60,22 +59,20 @@ function OnEvent(event, arg)
     end
     -- 换甲 end
 
-
     if (IsKeyLockOn("capslock") or DisableCapsLock) then
         while (IsMouseButtonPressed(3)) do
-            if(IsMouseButtonPressed(1)) then
-
-                MoveMouseRelative(-Range, Range )
+            if (IsMouseButtonPressed(1)) then
+                MoveMouseRelative(-Range, Range)
                 BetterSleep(Frequency)
-                MoveMouseRelative(Range, -Range )
+                MoveMouseRelative(Range, -Range)
                 BetterSleep(Frequency)
-                MoveMouseRelative(-Range, Range )
+                MoveMouseRelative(-Range, Range)
                 BetterSleep(Frequency)
-                MoveMouseRelative(Range, -Range )
+                MoveMouseRelative(Range, -Range)
                 BetterSleep(Frequency)
-                MoveMouseRelative(-Range, Range )
+                MoveMouseRelative(-Range, Range)
                 BetterSleep(Frequency)
-                MoveMouseRelative(Range, -Range )
+                MoveMouseRelative(Range, -Range)
                 BetterSleep(Frequency)
                 if (PressGun) then
                     MoveMouseRelative(0, Decline)
